5dcaed72891905346116666a602179177626c9b3
harinipalraj <harinipalraj05@gmail.com>
2025-02-22 14:17:11 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,3 @@
+a={"name":"harini","age":81,"college":"vysya"}
+a.popitem()
+print(a)
--- set.py
+++ set.py
@@ -1,1 +1,5 @@
-a={"harini","lenovo","redmi",""}
+a={"harini","lenovo","redmi","harini",1,9,5}
+b={5555,3456,34}
+print(len(a))
+a.clear()
+print(a)
