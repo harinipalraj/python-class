ed65be8d5c84d53c93d95282b46bf9cbb410a25b
harinipalraj <harinipalraj05@gmail.com>
2025-03-21 21:36:59 +0530

    Add files via upload

---
+++ harini try.py
@@ -0,0 +1,7 @@
+input1=input("enter your option:")
+if input1=="stone":
+    print("The user selected stone")
+elif input1=="scissor":
+    print("The user selected scissor")
+elif input1=="paper":
+    print("The user selected paper")
